82b4e5711940e7d0b416436a9e4d20f21b2e8a27
pistazie <tom@pistazie.rocks>
2022-12-20 14:08:34 +0100

    example 4

--- script.sh
+++ script.sh
@@ -1,7 +1,6 @@
-RUN=3
+RUN=4
 git switch -c branch$RUN
 echo $RUN$RUNN$RUNN$RUNN$RUNN$RUN > README.md
 git commit -a -m "example ${RUN}"
 git push --set-upstream origin branch$RUN
-git push
 gh pr create -f
